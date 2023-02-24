796563b5829f1bed07a59964f93b625a5cd0f674
Nicole J <nicole.a.jordaan@gmail.com>
2023-02-24 16:33:15 +0000

    connect to and from database

--- src/main/java/com/napier/maxwell/Main.java
+++ src/main/java/com/napier/maxwell/Main.java
@@ -1,8 +1,77 @@
 package com.napier.maxwell;
 
-public class Main {
-    public static void main(String[] args) {
+import java.sql.*;
 
-        System.out.println("Hello world!");
+public class Main
+{
+    /**
+     *  Connection to MySQL database
+     */
+    private Connection con = null;
+    public static void main(String[] args)
+    {
+        // Create new Application
+        Main main = new Main();
+
+        // Connect to database
+        main.connect();
+
+        // Disconnect from database
+        main.disconnect();
+    }
+
+    /**
+     * Connect to MySQL database
+     */
+    public void connect()
+    {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database......");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect() {
+        if (con != null) {
+            try {
+                // Close connection
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error closing connection to database");
+            }
+        }
     }
 }
