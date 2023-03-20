84c5c09c7ff84f111d8ad0ad3da137642523617f
malkybar <40530828@live.napier.ac.uk>
2023-03-20 20:13:40 +0000

    Created Country Class

---
+++ src/main/java/com/napier/maxwell/Country.java
@@ -0,0 +1,82 @@
+package com.napier.maxwell;
+
+/**
+ * Class to represent a country
+ */
+public class Country {
+
+    /**
+     * Country Code
+     */
+    public String Code;
+
+    /**
+     * Country Name
+     */
+    public String Name;
+
+    /**
+     * Country Continent
+     */
+    public String Continent;
+
+    /**
+     * Country Region
+     */
+    public String Region;
+
+    /**
+     * Country Surface Area
+     */
+    public double SurfaceArea;
+
+    /**
+     * Country Independence Year
+     */
+    public int IndepYear;
+
+    /**
+     * Country Population
+     */
+    public int Population;
+
+    /**
+     * Country Life Expectancy
+     */
+    public double LifeExpectancy;
+
+    /**
+     * Country GNP
+     */
+    public double GNP;
+
+    /**
+     * Country GNP Old
+     */
+    public double GNPOld;
+
+    /**
+     * Country LocalName
+     */
+    public String LocalName;
+
+    /**
+     * Country Government Form
+     */
+    public String GovernmentForm;
+
+    /**
+     * Country Head of State
+     */
+    public String HeadOfState;
+
+    /**
+     * Country Capital
+     */
+    public String Capital;
+
+    /**
+     * Country Code2
+     */
+    public String Code2;
+}
