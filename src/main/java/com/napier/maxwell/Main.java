6446565b6b5d8940875e86651d8638ed042890e7
SWeasul <40489185@live.napier.ac.uk>
2023-03-24 02:38:49 +0000

    Prepping integration tests.

--- src/main/java/com/napier/maxwell/Main.java
+++ src/main/java/com/napier/maxwell/Main.java
@@ -876,7 +876,7 @@ public class Main
                 Thread.sleep(30000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://" + location
-                                + "/employees?allowPublicKeyRetrieval=true&useSSL=false",
+                                + "/world?allowPublicKeyRetrieval=true&useSSL=false",
                                 "root", "example");
                 System.out.println("Successfully connected");
                 break;
