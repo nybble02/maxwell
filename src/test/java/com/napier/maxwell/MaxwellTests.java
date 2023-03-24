fc44206413d1a301322b456214b77a752f4cff12
SWeasul <40489185@live.napier.ac.uk>
2023-03-24 01:29:27 +0000

    Giving up soon...

--- src/test/java/com/napier/maxwell/MaxwellTests.java
+++ src/test/java/com/napier/maxwell/MaxwellTests.java
@@ -1,15 +1,9 @@
+package com.napier.maxwell;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.napier.maxwell.City;
-import com.napier.maxwell.Country;
-import com.napier.maxwell.Main;
-import java.util.ArrayList;
 
-
-
-
-class MaxwellTesting
+class MyTest
 {
     @Test
     void unitTest()
